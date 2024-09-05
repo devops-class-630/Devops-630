778aa32aa76cfef465543a44822911dd369b42db
DeepakBomjan <44976635+DeepakBomjan@users.noreply.github.com>
2024-09-05 08:25:01 +0545

    ci: fix deployment

--- scripts/IaC/Terraform/ec2.tf
+++ scripts/IaC/Terraform/ec2.tf
@@ -1,14 +1,14 @@
 provider "aws" {
 region = "us-east-1"
 }
-terraform {
-  backend "s3" {
-    bucket         = "devops630newworldlab"
-    key            = "terraform.tfstate"
-    region         = "us-east-1"
-    encrypt        = true
-  }
-}
+#terraform {
+#  backend "s3" {
+#    bucket         = "devops630newworldlab"
+#    key            = "terraform.tfstate"
+#    region         = "us-east-1"
+#    encrypt        = true
+#  }
+#}
 
 
 data "aws_ami" "ubuntu" {
@@ -27,9 +27,52 @@ filter {
 owners = ["099720109477"] # Canonical
 }
 
+data "aws_vpc" "default" {
+  default = true
+}
+
+resource "aws_security_group" "allow_ssh_http" {
+  name        = "allow_ssh_http"
+  description = "Security group for SSH and HTTP access"
+  vpc_id      = data.aws_vpc.default.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # Allow SSH from anywhere (adjust as necessary)
+  }
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # Allow HTTP from anywhere (adjust as necessary)
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_ssh_http"
+  }
+}
+
+resource "aws_key_pair" "ubuntu_ssh_key" {
+  key_name = "my_ubuntu_ssh_key"
+  public_key = file("./id_rsa.pub")
+  
+}
+
 resource "aws_instance" "web" {
 ami           = data.aws_ami.ubuntu.id
 instance_type = "t3.micro"
+key_name      = "my_ubuntu_ssh_key"
+vpc_security_group_ids = [aws_security_group.allow_ssh_http.id]
 
 tags = {
   Name = "My-ec2-lab-machine"
@@ -50,3 +93,10 @@ output "instance_id" {
 
 }
 
+output "security_group_id" {
+  value = aws_security_group.allow_ssh_http.id
+}
+
+output "default_vpc_id" {
+  value = data.aws_vpc.default.id
+}
